2f4ac366fbebf5087ee6349b7044bf01c3a6a70a
singuerinc <nahuel.scotti@gmail.com>
2018-08-24 09:13:55 +0200

    fix: lambda functions #wip

--- api.ts
+++ api.ts
@@ -4,5 +4,4 @@ const get = (x, url, params) => {
   return x.get(url, params);
 };
 
-export const request = (url, params) =>
-  get(axios, `https://cors-anywhere.herokuapp.com/${url}`, params);
+export const request = (url, params) => get(axios, url, params);
--- lambda/fetch.js
+++ lambda/fetch.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const API_ENDPOINT = "https://www.artstation.com/projects.json";
 
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data
+      body: JSON.stringify(data)
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
