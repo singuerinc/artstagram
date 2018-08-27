58b987bcb8c863190b342aa50b51c79a6ac363f2
singuerinc <nahuel.scotti@gmail.com>
2018-08-27 13:31:26 +0200

    feat: awesome menu

--- src/nav.tsx
+++ src/nav.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ifElse, equals, always } from "ramda";
+import { icons } from "feather-icons";
 
 const Nav = ({ sorting }) => {
   const selectedWhen = ifElse(equals(), always("selected"), always(null))(
@@ -9,16 +10,40 @@ const Nav = ({ sorting }) => {
   return (
     <ul className="nav">
       <li className={selectedWhen("latest")}>
-        <a href="?sorting=latest">Latest</a>
+        <a href="?sorting=latest">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: icons.zap.toSvg()
+            }}
+          />
+        </a>
       </li>
       <li className={selectedWhen("picks")}>
-        <a href="?sorting=picks">Picks</a>
+        <a href="?sorting=picks">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: icons.award.toSvg()
+            }}
+          />
+        </a>
       </li>
       <li className={selectedWhen("trending")}>
-        <a href="?sorting=trending">Trending</a>
+        <a href="?sorting=trending">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: icons["trending-up"].toSvg()
+            }}
+          />
+        </a>
       </li>
       <li className={selectedWhen("randomize")}>
-        <a href="?sorting=randomize">Community</a>
+        <a href="?sorting=randomize">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: icons.users.toSvg()
+            }}
+          />
+        </a>
       </li>
     </ul>
   );
