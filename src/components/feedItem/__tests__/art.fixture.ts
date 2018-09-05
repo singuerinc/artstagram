39926264f20005cd6f07bbc30c81266e287a7ba2
singuerinc <nahuel.scotti@gmail.com>
2018-09-05 19:17:42 +0200

    test: FeedItemHeader tests

---
+++ src/components/feedItem/__tests__/art.fixture.ts
@@ -0,0 +1,64 @@
+import { IArtImage } from "../../../IArtImage";
+
+export const art: IArtImage = {
+  id: 3639053,
+  user_id: 316724,
+  title: "Cerulea Cyandr [Reference Sheet]",
+  description:
+    "My first character reference sheet! \r\nCerulea is the Pilot of the Hachidori EX (https://www.artstation.com/artwork/Ln0Dw)",
+  created_at: "2018-09-05T08:12:10.994-05:00",
+  updated_at: "2018-09-05T08:14:44.349-05:00",
+  likes_count: 0,
+  slug: "cerulea-cyandr-reference-sheet",
+  published_at: "2018-09-05T08:14:44.346-05:00",
+  adult_content: false,
+  cover_asset_id: 12713977,
+  admin_adult_content: false,
+  views_count: 0,
+  hash_id: "lgWek",
+  permalink: "https://www.artstation.com/artwork/lgWek",
+  hide_as_adult: false,
+  user: {
+    id: 316724,
+    username: "pyrokidsosa",
+    first_name: "Aisosa",
+    last_name: "Ugiagbe",
+    avatar_file_name: "b8fe77f2f93a88f7047566bb8f9b2301.png",
+    country: "United Kingdom",
+    city: "London",
+    subdomain: "pyrokidsosa",
+    headline: "Digital Artist, Illustrator, Character artist/designer",
+    pro_member: false,
+    is_staff: false,
+    medium_avatar_url:
+      "https://cdna.artstation.com/p/users/avatars/000/316/724/medium/b8fe77f2f93a88f7047566bb8f9b2301.jpg",
+    large_avatar_url:
+      "https://cdna.artstation.com/p/users/avatars/000/316/724/large/b8fe77f2f93a88f7047566bb8f9b2301.jpg",
+    full_name: "Aisosa Ugiagbe",
+    permalink: "http://www.artstation.com/pyrokidsosa",
+    artstation_profile_url: "https://www.artstation.com/pyrokidsosa",
+    location: "London, United Kingdom"
+  },
+  cover: {
+    id: 12713977,
+    small_image_url:
+      "https://cdnb.artstation.com/p/assets/images/images/012/713/977/small/aisosa-ugiagbe-1.jpg?1536153239",
+    medium_image_url:
+      "https://cdnb.artstation.com/p/assets/images/images/012/713/977/medium/aisosa-ugiagbe-1.jpg?1536153239",
+    small_square_url:
+      "https://cdnb.artstation.com/p/assets/images/images/012/713/977/20180905081358/small_square/aisosa-ugiagbe-1.jpg?1536153239",
+    thumb_url:
+      "https://cdnb.artstation.com/p/assets/images/images/012/713/977/20180905081358/smaller_square/aisosa-ugiagbe-1.jpg?1536153239",
+    micro_square_image_url:
+      "https://cdnb.artstation.com/p/assets/images/images/012/713/977/20180905081358/micro_square/aisosa-ugiagbe-1.jpg?1536153239",
+    aspect: 1.38
+  },
+  icons: {
+    image: false,
+    video: false,
+    model3d: false,
+    marmoset: false,
+    pano: false
+  },
+  assets_count: 1
+};
