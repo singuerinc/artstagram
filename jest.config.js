87e2f8fa82d0c8b668b21ea2262c399a4c4de946
singuerinc <nahuel.scotti@gmail.com>
2018-09-10 16:31:51 +0200

    fix: test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   rootDir: "./src/",
   setupTestFrameworkScriptFile: "<rootDir>/setupTests.ts",
-  moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
   },
+  testMatch: ["**/__tests__/*.(test|spec).(ts|tsx|js)"],
+
   transform: {
-    "^.+\\.(ts|tsx)$": "<rootDir>/../config/test-preprocessor.js"
-  },
-  testMatch: ["**/__tests__/*.(test|spec).(ts|tsx|js)"]
+    "^.+\\.tsx?$": "ts-jest"
+  }
 };
